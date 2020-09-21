5e61427671b840caaccab06727f9885aab92998f
JG-Dev <ljg615@nate.com>
2020-09-21 20:39:49 +0900

    - jsp 설정추가

---
+++ src/main/java/com/jg/toy/springreact/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.jg.toy.springreact.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
+}
