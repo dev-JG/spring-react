98b614a7990d52b7b8008991072baead18f487c0
JG-Dev <ljg615@nate.com>
2020-09-23 18:03:48 +0900

    - build 추가

--- src/main/java/com/jg/toy/springreact/controller/HomeController.java
+++ src/main/java/com/jg/toy/springreact/controller/HomeController.java
@@ -1,16 +1,23 @@
 package com.jg.toy.springreact.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
 @Controller
 @RequestMapping("")
-public class HomeController {
+public class HomeController implements ErrorController {
 
-    @RequestMapping(value="/")
+    @GetMapping({"/", "/error"})
     public String index() {
-        return "index";
+        return "index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
